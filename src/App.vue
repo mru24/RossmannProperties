e44d34610068ba5b2cf4cfb439d7012df6b4e556
mru24 <mru24@yahoo.com>
2018-11-08 18:02:30 +0000

    Edited

--- src/App.vue
+++ src/App.vue
@@ -10,16 +10,6 @@ import navbar from './components/navbar'
 export default {
   components: {
     navbar
-  },
-  methods: {
-    alert: function (e) {
-      alert(e)
-    }
-  },
-  created () {
-    window.addEventListener('resize', function () {
-      this.alert('Please refreash your browser after resizing')
-    })
   }
 }
 </script>
